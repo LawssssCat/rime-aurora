582fafe8179a98f544a5453f44e1d563f91cdb7b
LawssssCat <1191693505@qq.com>
2022-08-24 21:23:06 +0800

    feat: rq=>日期 sj=>时间

--- rime.lua
+++ rime.lua
@@ -6,3 +6,20 @@ select_character_processor = require("select_character")
 -- 详见 `lua/easy_en.lua`
 local easy_en = require("easy_en")
 easy_en_enhance_filter = easy_en.enhance_filter
+
+-- rq输出日期、sj输出时间
+-- 参考：
+-- https://github.com/LEOYoon-Tsaw/Rime_collections/blob/master/Rime_description.md#%E7%A4%BA%E4%BE%8B-9
+-- https://www.zhihu.com/question/268770492/answer/2190114796
+function time_translator(input, seg, env)
+  if (input == "rq") then
+    local cand = Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), "")
+    cand.quality = 1
+    yield(cand)
+  end
+  if (input == "sj") then
+    local cand = Candidate("time", seg.start, seg._end, os.date("%H:%M"), " ")
+    cand.quality = 1
+    yield(cand)
+  end
+end
