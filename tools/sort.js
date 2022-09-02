51ee5d116f211430564413acaf4b689abde2bfd7
LawssssCat <1191693505@qq.com>
2022-09-02 18:29:07 +0800

    improvement: opencc/kemoji.json

--- tools/sort.js
+++ tools/sort.js
@@ -5,14 +5,22 @@ const path = require('path');
 const baseDir = path.join(__dirname, '../'); // 根目录
 
 const openccBase = path.join(baseDir, 'opencc');
+// emoji 表情
 dealDuplication.handleOpenccFiles([
   path.join(openccBase, 'emoji_category.txt'),
   path.join(openccBase, 'emoji_word.txt'),
-  path.join(openccBase, 'kemoji_base.txt'),
-  path.join(openccBase, 'kemoji_meow.txt'),
 ], 
 path.join(openccBase, 'emoji_all.txt'), // output
 (record) => {
-  console.log(record, 'opencc处理完成！');
+  console.log(record, 'opencc_emoji 处理完成！');
+});
+// kemoji 颜表情
+dealDuplication.handleOpenccFiles([
+  path.join(openccBase, 'kemoji_base.txt'),
+  path.join(openccBase, 'kemoji_meow.txt'),
+], 
+path.join(openccBase, 'kemoji_all.txt'), // output
+(record) => {
+  console.log(record, 'opencc_kemoji 处理完成！');
 });
 
