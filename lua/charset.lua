67507a4074c93971334380cca775cfb007a83f91
骆言 <1191693505@qq.com>
2022-09-12 14:35:07 +0800

    improvement(lua charset): 展示编码，只为单字展示

--- lua/charset.lua
+++ lua/charset.lua
@@ -13,57 +13,31 @@
 
 --]]
 
+local string_helper = require("tools/string_helper")
+
 local charset = {
-  ["CJK"] = { first = 0x4E00, last = 0x9FFF },     -- CJK Unified Ideographs - https://unicode.org/charts/PDF/U4E00.pdf
-  ["ExtA"] = { first = 0x3400, last = 0x4DBF },    -- CJK Unified Ideographs Extension A - https://unicode.org/charts/PDF/U3400.pdf
-  ["ExtB"] = { first = 0x20000, last = 0x2A6DF },  -- CJK Unified Ideographs Extension B - https://unicode.org/charts/PDF/U20000.pdf
-  ["ExtC"] = { first = 0x2A700, last = 0x2B73F },  -- CJK Unified Ideographs Extension C - https://unicode.org/charts/PDF/U2A700.pdf
-  ["ExtD"] = { first = 0x2B740, last = 0x2B81F },  -- CJK Unified Ideographs Extension D - https://unicode.org/charts/PDF/U2B740.pdf
-  ["ExtE"] = { first = 0x2B820, last = 0x2CEAF },  -- CJK Unified Ideographs Extension E - https://unicode.org/charts/PDF/U2B820.pdf
-  ["ExtF"] = { first = 0x2CEB0, last = 0x2EBEF },  -- CJK Unified Ideographs Extension F - https://unicode.org/charts/PDF/U2CEB0.pdf
-  ["ExtG"] = { first = 0x30000, last = 0x3134A },  -- CJK Unified Ideographs Extension G - https://unicode.org/charts/PDF/U30000.pdf
-  ["Compat"] = { first = 0x2F800, last = 0x2FA1F } -- CJK Compatibility Ideographs Supplement - https://unicode.org/charts/PDF/U2F800.pdf
+  { name = "CJK", first = 0x4E00, last = 0x9FFF },     -- CJK Unified Ideographs - https://unicode.org/charts/PDF/U4E00.pdf
+  { name = "ExtA", first = 0x3400, last = 0x4DBF },    -- CJK Unified Ideographs Extension A - https://unicode.org/charts/PDF/U3400.pdf
+  { name = "ExtB", first = 0x20000, last = 0x2A6DF },  -- CJK Unified Ideographs Extension B - https://unicode.org/charts/PDF/U20000.pdf
+  { name = "ExtC", first = 0x2A700, last = 0x2B73F },  -- CJK Unified Ideographs Extension C - https://unicode.org/charts/PDF/U2A700.pdf
+  { name = "ExtD", first = 0x2B740, last = 0x2B81F },  -- CJK Unified Ideographs Extension D - https://unicode.org/charts/PDF/U2B740.pdf
+  { name = "ExtE", first = 0x2B820, last = 0x2CEAF },  -- CJK Unified Ideographs Extension E - https://unicode.org/charts/PDF/U2B820.pdf
+  { name = "ExtF", first = 0x2CEB0, last = 0x2EBEF },  -- CJK Unified Ideographs Extension F - https://unicode.org/charts/PDF/U2CEB0.pdf
+  { name = "ExtG", first = 0x30000, last = 0x3134A },  -- CJK Unified Ideographs Extension G - https://unicode.org/charts/PDF/U30000.pdf
+  { name = "Compat", first = 0x2F800, last = 0x2FA1F } -- CJK Compatibility Ideographs Supplement - https://unicode.org/charts/PDF/U2F800.pdf
 }
 
-local function exists(single_filter, text)
-  for i in utf8.codes(text) do
-    local c = utf8.codepoint(text, i)
-    if (not single_filter(c)) then
-      return false
+local function get_utf8_code(c)
+  for index, obj in pairs(charset) do
+    local code = utf8.codepoint(c, 1)
+    if(code >= obj.first and code <= obj.last) then
+      return {
+        code = code,
+        name = obj.name
+      }
     end
   end
-  return true
-end
-
-local function is_charset(s)
-  return function (c)
-    return c >= charset[s].first and c <= charset[s].last
-  end
-end
-
-local function is_cjk_ext(c)
-  return is_charset("ExtA")(c) or is_charset("ExtB")(c) or
-    is_charset("ExtC")(c) or is_charset("ExtD")(c) or
-    is_charset("ExtE")(c) or is_charset("ExtF")(c) or
-    is_charset("Compat")(c)
-end
-
---[[
-滤除含 CJK 扩展汉字的候选项：
---]]
-local function charset_filter(input)
-  -- 使用 `iter()` 遍历所有输入候选项
-  for cand in input:iter() do
-    -- 如果当前候选项 `cand` 不含 CJK 扩展汉字
-    if (not exists(is_cjk_ext, cand.text))
-    then
-      -- 结果中仍保留此候选
-      yield(cand)
-    end
-    --[[ 上述条件不满足时，当前的候选 `cand` 没有被 yield。
-      因此过滤结果中将不含有该候选。
-    --]]
-  end
+  return nil
 end
 
 --[[
@@ -73,23 +47,23 @@ local function charset_comment_filter(input, env)
   local option = env.engine.context:get_option("option_charset_comment_filter") -- 开关
   -- 使用 `iter()` 遍历所有输入候选项
   for cand in input:iter() do
-    if(option)
+    local text = cand.text
+    local len = utf8.len(cand.text)
+    local flag01 = not string.match(text, "^[%w]*$") -- 非纯字母/数字
+    if(option and flag01 and len == 1) -- 只对单字显示编码
+    -- if(option and flag01)
     then
-      -- 判断当前候选内容 `cand.text` 中文字属哪个字符集
-      -- s key
-      -- r value
-      for s, r in pairs(charset) do
-        if (exists(is_charset(s), cand.text)) then
-        --[[ 修改候选的注释 `cand.comment`
-          因复杂类型候选项的注释不能被直接修改，
-          因此使用 `get_genuine()` 得到其对应真实的候选项
-        --]]
-          local str_cjk = "|" .. s .. "|"
-          if(string.find(cand.comment, str_cjk) == nil) then
-            cand:get_genuine().comment = str_cjk .. " " .. cand.comment
-          end
-          break
+      local m_arr = {}
+      for i = 1, len do
+        local c = string_helper.sub(text, i, i)
+        local m = get_utf8_code(c)
+        if(m) then
+          table.insert(m_arr, c .. "=" .. string.format("0x%x", m.code) .. "(" .. m.name .. ")")
         end
+      end
+      if(#m_arr > 0) then
+        local comment = "|" .. table.concat(m_arr, ", ") .. "| "
+        cand:get_genuine().comment = comment .. cand.comment
       end
     end -- option
     -- 在结果中对应产生一个带注释的候选
@@ -98,6 +72,5 @@ local function charset_comment_filter(input, env)
 end
 
 return { 
-  filter = charset_filter,
   comment_filter = charset_comment_filter 
 }
--- rime.lua
+++ rime.lua
@@ -95,11 +95,11 @@ time_translator = require("time_translator")
 --]]
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+-- 对候选词做处理，参考：https://github.com/hchunhui/librime-lua/blob/master/sample/lua/charset.lua
 -- charset_filter: 滤除含 CJK 扩展汉字的候选项
 -- charset_comment_filter: 为候选项加上其所属字符集的注释
 -- 详见 `lua/charset.lua`
 local charset = require("charset")
-charset_filter = charset.filter
 charset_comment_filter = charset.comment_filter
 
 -- single_char_filter: 候选项重排序，使单字优先
