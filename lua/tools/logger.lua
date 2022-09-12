d5f50308e4e2781deca57705080458b2efee4fbb
骆言 <1191693505@qq.com>
2022-09-12 21:40:49 +0800

    fix: gsub 报错，当 info.name == nil

--- lua/tools/logger.lua
+++ lua/tools/logger.lua
@@ -10,26 +10,10 @@ local WARN = "warn"
 local ERROR = "error"
 local TRACE = "trace"
 
-local pattern = "{level} {time} {path}:{line} {method}] {msg}"
-
-local MODE_DEBUG_ON = 1
-local MODE_DEBUG_OFF = 0
-MODE_DEBUG = MODE_DEBUG or MODE_DEBUG_ON
+local pattern = "{level} {path}:{line} {method}] {msg}"
+-- local pattern = "{level} {time} {path}:{line} {method}] {msg}"
 
 local logger = {} -- return 
-
--- 调试模式 判断
-function logger.isDebug()
-  return MODE_DEBUG_ON == MODE_DEBUG
-end
--- 调试模式 开
-function logger.setDebugOn()
-  MODE_DEBUG = MODE_DEBUG_ON
-end
--- 调式模式 关
-function logger.setDebugOff()
-  MODE_DEBUG = MODE_DEBUG_OFF
-end
 
 local function format(level, info, ...)
   local arr = {}
@@ -40,8 +24,12 @@ local function format(level, info, ...)
   local result = pattern
   result = string.gsub(result, "{level}", string.upper(level))
   result = string.gsub(result, "{time}", os.date("%Y%m%d %H:%M:%S"))
-  result = string.gsub(result, "{path}", info.short_src)
-  result = string.gsub(result, "{method}", info.name)
+  result = string.gsub(result, "{path}", info.short_src:match(".?(lua[\\/].+)$"))
+  if(info.name) then
+    result = string.gsub(result, "{method}", info.name)
+  else
+    result = string.gsub(result, "%s*{method}", "") -- 管理前分割符
+  end
   result = string.gsub(result, "{line}", info.currentline)
   result = string.gsub(result, "{msg}", msg)
   return result
